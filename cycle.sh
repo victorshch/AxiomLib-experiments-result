b642e9b35c5f20910695f3424b11dbffe179c564
Timur Bilalov <t.bilalov@hh.ru>
2015-04-11 11:34:40 +0300

    add prefix to dataset name

--- cycle.sh
+++ cycle.sh
@@ -3,28 +3,28 @@
 main_repo="/home/bilalov/study/experiments"
 generator="/home/bilalov/study/time-series-generator"
 
-START=$1
-END=100
-PREFIX=$2
+START=0
+END=50
+PREFIX=$1
 
 if [ $# -eq 0 ]
 then 
-	START=0
 	PREFIX=''
 fi
 
 for ((i=START; i<END; i++))
 do
-	echo 'create dataset'$i
+	dataset=$PREFIX'dataset'$i
+	echo 'create '$dataset
 	cd $generator
-	$generator/generate_axiomlib.sh $PREFIXdataset$i
+	$generator/generate_axiomlib.sh "$dataset"
 	cd $main_repo
 	echo 'run clust simple'
-	./run_experiment.sh config.clust.simple.cfg	$generator/$PREFIXdataset$i
+	./run_experiment.sh config.clust.simple.cfg	$generator/"$dataset"
 	echo 'run clust genetic'
-	./run_experiment.sh config.clust.genetic.cfg $generator/$PREFIXdataset$i	
+	./run_experiment.sh config.clust.genetic.cfg $generator/"$dataset"	
 	echo 'run simple genetic'
-	./run_experiment.sh config.simple.genetic.cfg $generator/$PREFIXdataset$i	
+	./run_experiment.sh config.simple.genetic.cfg $generator/"$dataset"	
 	echo 'run simple simple'
-	./run_experiment.sh config.simple.simple.cfg $generator/$PREFIXdataset$i	
+	./run_experiment.sh config.simple.simple.cfg $generator/"$dataset"	
 done
